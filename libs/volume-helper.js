16ebb70353865847c88dad4630b9f5f21fce2266
Jon Reiling <jon.reiling@akqa.com>
2015-12-22 09:57:18 -0500

    Added additional cases for volume

--- libs/volume-helper.js
+++ libs/volume-helper.js
@@ -17,9 +17,35 @@ VolumeHelper.prototype = Object.create(events.EventEmitter.prototype, {
 
 VolumeHelper.prototype.setVolume = function( volume ) {
 
+	switch (volume) {
+
+		case 'up':
+			volume = this.currentVolume + 10
+			break;
+
+		case 'down':
+			volume = this.currentVolume - 10;
+			break;		
+
+		case 'normal':
+			volume = 50;
+			break;
+
+		case 'low':
+			volume = 15;
+			break;
+
+		default:
+
+			break;
+
+	}
+
+	//constrain the volume between 0-100. 
+	volume = Math.max( 0 , Math.min( 100 , volume) );
+
 	this.perceivedVolume = volume;
 	var targetVol = 40 * (volume/100) + 60;
-	if ( targetVol > 100 ) targetVol = 100;
 	
 	if ( targetVol != this.currentVolume ) {
 		this.emit( 'volumeUpdate' , this.perceivedVolume );
